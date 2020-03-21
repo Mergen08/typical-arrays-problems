b78e5e7471be1a8ae8877441b8ee7307c1ad2c1d
Mergen08 <60552356+Mergen08@users.noreply.github.com>
2020-03-22 00:10:12 +0300

    init: new solve

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-
+const isArrValid = (array) => {
+  if (array === undefined) return false;
+  if (array.length === 0) return false;
+  return true;
+}
 exports.min = function min (array) {
+  if (isArrValid(array)) return Math.min(...array);
   return 0;
 }
 
 exports.max = function max (array) {
+  if (isArrValid(array)) return Math.max(...array);
   return 0;
 }
-
 exports.avg = function avg (array) {
+  if (isArrValid(array)) return array.reduce((sum, num) => {
+    sum += num;
+    return sum;
+  }, 0) / array.length;
   return 0;
 }
