8cb344f7491ac8ab0e9323f8184fbda03adee1d4
Mergen08 <60552356+Mergen08@users.noreply.github.com>
2020-03-22 00:41:28 +0300

    fix: new solve2

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
-const isArrValid = (array) => {
-  if (array === undefined) return false;
-  if (array.length === 0) return false;
-  return true;
+function checkout(array) {
+    if (!array || !array.length) {
+        return false;
+    }
+    return true;
 }
-exports.min = function min (array) {
-  if (isArrValid(array)) return Math.min(...array);
-  return 0;
-}
-
-exports.max = function max (array) {
-  if (isArrValid(array)) return Math.max(...array);
-  return 0;
-}
-exports.avg = function avg (array) {
-  if (isArrValid(array)) return array.reduce((sum, num) => {
-    sum += num;
-    return sum;
-  }, 0) / array.length;
-  return 0;
-}
+exports.min = function min(array) {
+    if (!checkout(array)) return 0;
+    else return Math.min.apply(null, array);
+};
+exports.max = function max(array) {
+    if (!checkout(array)) return 0;
+    else return Math.max.apply(null, array);
+};
+exports.avg = function avg(array) {
+    if (!checkout(array)) return 0;
+    else return array.reduce((a, b) => a + b) / array.length;
+};
